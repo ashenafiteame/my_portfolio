f2906e613036eb017f2877395791f448eb97f278
ashu <ashenafitm@gmail.com>
2023-03-21 19:45:12 -0500

    git hub action added

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -3,20 +3,20 @@ import Project from "../../components/Project";
 import PageHeader from "../../components/PageHeader";
 
 const Portfolio = () => {
-  const ProjectList = () =>
-    projectData.map((project, i) => (
-      <Project
-        key={i}
-        id={project.id}
-        title={project.title}
-        technologies={project.technologies}
-        image={project.image}
-        color={project.bgcolor}
-        github={project.github}
-        deployed={project.deployed}
-        description={project.description}
-      />
-    ));
+  // const ProjectList = () =>
+  projectData.map((project, i) => (
+    <Project
+      key={i}
+      id={project.id}
+      title={project.title}
+      technologies={project.technologies}
+      image={project.image}
+      color={project.bgcolor}
+      github={project.github}
+      deployed={project.deployed}
+      description={project.description}
+    />
+  ));
 
   return (
     <section className="portfolio">
